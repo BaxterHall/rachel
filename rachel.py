79d8ede4630cebcf06356b09cdc05d6704e3b6e7
Baxter Hall <abaxterhall@gmail.com>
2020-04-29 22:47:22 -0400

    rachel.py

---
+++ rachel.py
@@ -0,0 +1,10 @@
+import pyautogui
+script = "This is for Rachel, you big fat, white nasty, smelling fat bitchpip install pyautogui  Why you took me off the motherfuking schedule with your trifling dirty white racist ass big fat bitch And maluma body ass bitch I'm coming up there and I'm going to beat the fuck Out of you bitching don't even call the police today Cause I'm gonna up there unexpected and wait on your motherfucking Ass bitch I'm coming to beat the fuck out of you bitch cause you did That on purpose with your aundry racist white ass thin haired bitch Watch I'm coming up there to fuck you up bitch I'm telling you Watch I know it kind of car you drive and I'm gonna wait on you and I'm going to beat your ass bitch"
+
+for x in script.split():
+    pyautogui.write(x)
+    pyautogui.press("enter")
+
+
+
+
